b8d8c6ba85efddf4a27e09d5e6f2817048a8d4c1
Thomas Moritz <thomas.moritz@zalari.de>
2022-01-27 17:16:36 +0100

    Docker stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require( '@actions/core' );
 const github = require( '@actions/github' );
+const exec = require( '@actions/exec' );
 const fs = require( 'fs' );
-const { readdirSync } = require( 'fs' )
 
 function getDirectories( path ) {
   return fs.readdirSync( path ).filter( function( file ) {
@@ -14,13 +14,13 @@ try {
   const tagJson = JSON.parse( core.getInput( 'tag-json' ) );
   const imageName = core.getInput( 'image-name' );
   const dockerhubUsername = core.getInput( 'dockerhub-username' );
-
-  console.log( packageDir );
-  console.log( tagJson.tags );
+  // remove dummy image name from tag array
   const tagArray = tagJson.tags.map( tags => tags.replace( `${imageName}:`, '' ) );
   tagArray.push( 'latest' )
+  // all directories are also project names fro NX
   const projectNames = getDirectories( packageDir );
   const dockerCommands = [];
+  // building the docker commands
   projectNames.forEach( projectName => {
     const imageTags = tagArray.map( tag => `${dockerhubUsername}/${projectName}:${tag}` )
     const dockerBuildString = `docker build -f ${packageDir}/${projectName}/Dockerfile . -t ${imageTags.join( '  -t ' )}`
@@ -29,6 +29,22 @@ try {
     dockerCommands.push( dockerPushString );
   } );
   console.log( dockerCommands );
+  (async () => {
+
+    for ( let dockerCommand of dockerCommands ) {
+      await exec
+        .getExecOutput( 'dockerCommand', [], {
+          ignoreReturnCode: true
+        } )
+        .then( res => {
+          if ( res.stderr.length > 0 && res.exitCode != 0 ) {
+            throw new Error( `docker build failed with: ${res.stderr}` );
+          }
+        } );
+
+    }
+    console.log( 'this will print last' );
+  })();
   const time = (new Date()).toTimeString();
   core.setOutput( "time", time );
   // Get the JSON webhook payload for the event that triggered the workflow
