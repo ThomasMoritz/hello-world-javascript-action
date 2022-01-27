ce05cd793d5ab3e7684c6392668e16c304dd5b11
Thomas Moritz <thomas.moritz@zalari.de>
2022-01-27 15:46:17 +0100

    next try

--- dist/index.js
+++ dist/index.js
@@ -8470,16 +8470,25 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(1243);
 const github = __nccwpck_require__(3599);
+const directory = './';
+const fs = __nccwpck_require__(7147);
+
 
 try {
   // `who-to-greet` input defined in action metadata file
+  fs.readdir(directory, (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
+
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${JSON.parse(nameToGreet)}!`);
+  console.log(JSON.parse(nameToGreet));
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const directory = './';
+const fs = require('fs');
+
 
 try {
   // `who-to-greet` input defined in action metadata file
+  fs.readdir(directory, (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
+
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${JSON.parse(nameToGreet)}!`);
+  console.log(JSON.parse(nameToGreet));
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
